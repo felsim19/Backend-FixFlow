41022fbc55870544889ebb20bc27785c0fab2454
felsim19 <Felipesierra49@gmail.com>
2025-04-03 12:34:35 -0500

    changes

--- connection/config.py
+++ connection/config.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DB_URL = "mysql+mysqlconnector://db_admin:admin_adso*@192.168.100.6:3306/fixflow"
+load_dotenv() 
+'''DB_UR_Linux = "mysql+mysqlconnector://db_admin:admin_adso*@192.168.100.6:3306/fixflow"'''
+# Configuración de la base de datos
+DB_URL = f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_URL')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 engine = create_engine(DB_URL)
 sessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 base = declarative_base()
--- routes/company.py
+++ routes/company.py
@@ -9,15 +9,19 @@ from models.VaultOut import outVaultRegistration
 from models.recoveryPassword import PasswordRecovery
 from connection.config import get_db
 from utils import is_valid_mail, generate_pin
+from dotenv import load_dotenv
 import bcrypt
+import os
+
+load_dotenv()
 
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="fixflowcorporativo@gmail.com",
-    MAIL_PASSWORD="ddwrelbxmvvihxbp",
-    MAIL_FROM="fixflowcorporativo@gmail.com",
-    MAIL_PORT = 587,
-    MAIL_SERVER = "smtp.gmail.com",
+    MAIL_USERNAME= os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT =os.getenv("MAIL_PORT"),
+    MAIL_SERVER = os.getenv("MAIL_SERVER"),
     MAIL_FROM_NAME="Fixflow",
     MAIL_STARTTLS = True,
     MAIL_SSL_TLS = False,
